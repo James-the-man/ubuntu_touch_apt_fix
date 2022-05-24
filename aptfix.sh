4f730de5b80db2fc85fbddc9db58774a3418c03b
Paul <81930845+James-the-man@users.noreply.github.com>
2022-05-25 00:36:49 +0300

    Update aptfix.sh

--- aptfix.sh
+++ aptfix.sh
@@ -5,19 +5,24 @@ if test $(id -u) -ne 0; then
         exit 0;
 fi
 
+mount -o remount,rw /
+
 mkdir -p $(pwd)subsystem/var/cache/
 mkdir -p $(pwd)subsystem/var/log/
 mkdir -p $(pwd)subsystem/var/lib/
+
 cp -r /var/cache/apt $(pwd)subsystem/var/cache/apt
 cp -r /var/lib/apt $(pwd)subsystem/var/lib/apt
 cp -r /var/log/apt $(pwd)subsystem/var/log/apt
 cp -r /var/lib/dpkg $(pwd)subsystem/var/lib/dpkg
 cp -r /var/cache/apt $(pwd)subsystem/var/cache/apt
 cp /var/log/dpkg.log $(pwd)subsystem/var/log/dpkg.log
+
 rm -r /var/lib/apt
 rm -r /var/log/apt
 rm -r /var/lib/dpkg
 rm /var/log/dpkg.log
+
 ln -s $(pwd)subsystem/var/cache/apt /var/cache/apt
 ln -s $(pwd)subsystem/var/lib/apt /var/lib/apt
 ln -s $(pwd)subsystem/var/log/apt /var/log/apt
