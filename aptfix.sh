06f00ca16967bfd0f6215ea337138c2b1b790952
Paul <81930845+James-the-man@users.noreply.github.com>
2022-05-29 19:30:01 +0300

    Yyyyyyyyyoooooooooo

--- aptfix.sh
+++ aptfix.sh
@@ -5,15 +5,15 @@ if test $(id -u) -ne 0; then
         exit 0;
 fi
 
-mkdir -p /home/phablet/./subsystem/var/cache/
-mkdir -p /home/phablet/./subsystem/var/log/
-mkdir -p /home/phablet/./subsystem/var/lib/
+mkdir -p /home/phablet/.subsystem/var/cache/
+mkdir -p /home/phablet/.subsystem/var/log/
+mkdir -p /home/phablet/.subsystem/var/lib/
 
-cp -r /var/cache/apt /home/phablet/./subsystem/var/cache/
-cp -r /var/lib/apt /home/phablet/./subsystem/var/lib/
-cp -r /var/log/apt /home/phablet/./subsystem/var/log/
-cp -r /var/lib/dpkg /home/phablet/./subsystem/var/lib/
-cp /var/log/dpkg.log /home/phablet/./subsystem/var/log/
+cp -r /var/cache/apt /home/phablet/.subsystem/var/cache/
+cp -r /var/lib/apt /home/phablet/.subsystem/var/lib/
+cp -r /var/log/apt /home/phablet/.subsystem/var/log/
+cp -r /var/lib/dpkg /home/phablet/.subsystem/var/lib/
+cp /var/log/dpkg.log /home/phablet/.subsystem/var/log/
 
 rm -r /var/cache/apt
 rm -r /var/lib/apt
@@ -21,8 +21,8 @@ rm -r /var/log/apt
 rm -r /var/lib/dpkg
 rm /var/log/dpkg.log
 
-ln -s /home/phablet/./subsystem/var/cache/apt /var/cache/
-ln -s /home/phablet/./subsystem/var/lib/apt /var/lib/
-ln -s /home/phablet/./subsystem/var/log/apt /var/log/
-ln -s /home/phablet/./subsystem/var/lib/dpkg /var/lib/
-ln -s /home/phablet/./subsystem/var/log/dpkg.log /var/log/
+ln -s /home/phablet/.subsystem/var/cache/apt /var/cache/
+ln -s /home/phablet/.subsystem/var/lib/apt /var/lib/
+ln -s /home/phablet/.subsystem/var/log/apt /var/log/
+ln -s /home/phablet/.subsystem/var/lib/dpkg /var/lib/
+ln -s /home/phablet/.subsystem/var/log/dpkg.log /var/log/
